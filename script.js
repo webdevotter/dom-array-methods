1effaeb7f7cc97c238eb08bd85b3a934b0303a21
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2023-04-28 10:44:34 -0500

    Calculate Wealth - reduce()

--- script.js
+++ script.js
@@ -50,6 +50,17 @@ function showMillionaires() {
   updateDOM();
 }
 
+// Calculate total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
 // Add new object to data array
 function addData(obj) {
   data.push(obj);
@@ -80,3 +91,4 @@ addBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
 showBtn.addEventListener('click', showMillionaires);
+calcBtn.addEventListener('click', calculateWealth);
