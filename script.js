3eb140b236f519c74883f67d050cb206fd04de67
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2023-04-28 09:56:04 -0500

    Output Users
    forEach(0 & DOM Methods

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const main = document.getElementById('main');
-const addBtn = document.getElementById('add');
+const addBtn = document.getElementById('add-user');
 const doubleBtn = document.getElementById('double');
-const showBtn = document.getElementById('show');
+const showBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
-const calcBtn = document.getElementById('calculate');
+const calcBtn = document.getElementById('calculate-wealth');
 
 let data = [];
 
@@ -23,10 +23,33 @@ async function getRandomUser() {
     money: Math.floor(Math.random() * 1000000),
   };
 
-  console.log(newUser);
+  addData(newUser);
 }
 
 // Add new object to data array
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
+
+// Update DOM
+function updateDOM(provideData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+  provideData.forEach((newUser) => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${newUser.name}</strong>
+    ${formatMoney(newUser.money)}`;
+    main.appendChild(element);
+  });
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event listeners
+addBtn.addEventListener('click', getRandomUser);
